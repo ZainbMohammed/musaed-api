8ac8dbf406f3cbe2f4b3704d49e938bec41ac55b
Zinam Mohamed <pk.x3311@gmail.com>
2024-03-17 00:49:00 +0300

    connect with mongodb

---
+++ DB/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+// require('dotenv').config();
+
+
+const dbConnection = async () => {
+    try {
+      // Connect to the MongoDB cluster
+      await mongoose.connect("mongodb+srv://asma:asma1010@cluster0.ca5xjkp.mongodb.net/musaedDB?retryWrites=true&w=majority&appName=Cluster0");
+      console.log("Mongoose is connected successful");
+    } catch (e) {
+      // connection faield
+      console.log("could not connect");
+    }
+}
+
+module.exports = dbConnection;
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+
 const express = require('express');
+const dbConnection = require('./DB/dbConnection');
 
 const app = express();
+
+
+// connect to db
+dbConnection();
 app.get('/',(req,res)=>{
 
     res.send('Hello world');
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('app listing on port 3000');
 })
+
+
